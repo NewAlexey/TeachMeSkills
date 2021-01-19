343645993e9025d2cf9f929a5d1bef5375d8f08d
NewAlexey <alexey.krupenia@gmail.com>
2021-01-20 00:51:47 +0300

    upgraded homework

--- Homework 6/script/change_theme.js
+++ Homework 6/script/change_theme.js
@@ -4,8 +4,4 @@ themeButton.onclick = function () {
     let style = theme_css.getAttribute("href");
     if (style.includes("style/style-dark.css")) document.getElementById("theme_css").href = "style/style-light.css";
     else document.getElementById("theme_css").href = "style/style-dark.css";
-
-    let graph = graph_1.getAttribute("src");
-    if (graph.includes("_light.png")) document.getElementById("graph_1").src = "./img/graph_gender-age_dark.png";
-    else document.getElementById("graph_1").src = "./img/graph_gender-age_light.png";
 };

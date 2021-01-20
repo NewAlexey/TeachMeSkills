0588fc2fe365889fd0d808673297517618ff664e
NewAlexey <alexey.krupenia@gmail.com>
2021-01-21 00:09:00 +0300

    updated change-theme button

--- Homework 6/script/change_theme.js
+++ Homework 6/script/change_theme.js
@@ -1,7 +1,22 @@
 let themeButton = document.getElementById("button");
+let themeCss = document.getElementById("theme_css");
+
+if (localStorage.getItem('theme') == 'dark') { 
+    themeCss.href = "style/style-dark.css";
+}
+else {
+    themeCss.href = "style/style-light.css";
+    document.getElementById('checkbox').removeAttribute('checked');
+}
 
 themeButton.onclick = function () {
     let style = theme_css.getAttribute("href");
-    if (style.includes("style/style-dark.css")) document.getElementById("theme_css").href = "style/style-light.css";
-    else document.getElementById("theme_css").href = "style/style-dark.css";
+    if (style.includes("style/style-dark.css")) {
+        themeCss.href = "style/style-light.css";
+        localStorage.setItem('theme', 'light');
+    }
+    else {
+        themeCss.href = "style/style-dark.css";
+        localStorage.setItem('theme', 'dark');
+    }    
 };
